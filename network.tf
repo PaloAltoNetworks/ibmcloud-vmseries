8e12731d5eccc9d75b12e3897a9396d57bacd050
cperson-palo <72570064+cperson-palo@users.noreply.github.com>
2021-09-16 14:18:33 -0400

    Network.tf - commented out vnf_subnet3

--- network.tf
+++ network.tf
@@ -13,6 +13,6 @@ data "ibm_is_subnet" "vnf_subnet2"{
    identifier = var.subnet_id2
 }
 
-data "ibm_is_subnet" "vnf_subnet3"{
-   identifier = var.subnet_id3
-}
+#data "ibm_is_subnet" "vnf_subnet3"{
+#   identifier = var.subnet_id3
+#}
