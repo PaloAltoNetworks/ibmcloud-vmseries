6de6f92c0ac99bbb18e235f6578ab1e50b437306
cperson-palo <72570064+cperson-palo@users.noreply.github.com>
2021-09-22 11:03:58 -0400

    Update image-map.tf

--- image-map.tf
+++ image-map.tf
@@ -24,17 +24,19 @@ resource "random_uuid" "test" { }
 locals {
         image_map = {
 	
-	pa-vm-kvm-9-1-3-1 = {
+pa-vm-kvm-9-1-3-1 = {
     "us-south" = "r006-83c04bd0-f9c4-4e6d-bc0e-5b8618e66968"
-		"us-east" = "r014-29b4bed0-9398-4a42-9220-f2cef4f5f39d"
-		"eu-gb" = "r018-eefbfc05-af62-4b88-923d-a60eef249ec4"
+	"us-east" = "r014-29b4bed0-9398-4a42-9220-f2cef4f5f39d"
+	"eu-gb" = "r018-eefbfc05-af62-4b88-923d-a60eef249ec4"
     "eu-de" = "r010-9899a24b-8d34-43cd-9f85-8d248af4e005"
-		"jp-tok" = "r022-e37e9b23-e42c-46e6-a21a-6392c448f834"	
-		"eu-fr2" = "r030-c6a4d0ef-e88a-4bfd-a4b4-0835e065cdc7"
+	"jp-tok" = "r022-e37e9b23-e42c-46e6-a21a-6392c448f834"	
+	"eu-fr2" = "r030-c6a4d0ef-e88a-4bfd-a4b4-0835e065cdc7"
     "au-syd" = "r026-8d201ec3-6c48-45c4-9c61-ac1fd7720115"
     "ca-tor" = "r038-01f97815-9b3c-47c5-840c-f50d4ddc8ea5"
     "jp-osa" = "r034-a9430fad-9f49-49a7-ace5-c19db212a24d"
-        }     
+        }  
+		
+		
   pa-vm-kvm-10-0-6 = {
     "us-south" = "r006-a2d7b1fe-feef-45c7-9d43-176899fa699b"
     "us-east" = "r014-7eb6331a-7452-47cf-927f-8a521cf24ed1"
